c89f142709e62ad0e198cb925ca7db7c25c8e0ab
Jameson <jamesonexil777@gmail.com>
2024-11-04 19:53:34 -0600

    Generating a random word from a list and asking the user to enter a letter that could be in the random word

--- guessing_word_game.py
+++ guessing_word_game.py
@@ -5,4 +5,19 @@
 # The user will have a numbers of chances to guess the correct letters until the user have the correct word or runs out of chances.
 
 # Whenever the user guess the wrong letter one life will be deducted to total of lives the user have until the user runs out of lives before the user guess the correct word
+import random
+# importing the random modules so i can use the random function
+
+print("Welcome to the game where you guess a letter until you guess the correct word")
+words_list = ["apple", "jameson", "orlando", "computers", "Headphone"]
+# creating a list of words
+
+random_word = random.choice(words_list)
+# generating a random word
+print(random_word)
+
+guess_letter = input("Guess a letter, until you the guess all the correct letters in the word ")
+# asking the user to guess a letter with the input function
+print(guess_letter)
+
 
