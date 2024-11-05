d89dfbb3359823d04e9553ef01357f6bd919c1e7
Jameson <jamesonexil777@gmail.com>
2024-11-04 19:41:12 -0600

    Flow chart of a guessing game

---
+++ guessing_word_game.py
@@ -0,0 +1,8 @@
+# Recreating the hangman game for the purpose of understand the logic of creating the game.
+
+# In the guessing word game, a random word is generated and the user is ask to guess the letters that make up the word.
+
+# The user will have a numbers of chances to guess the correct letters until the user have the correct word or runs out of chances.
+
+# Whenever the user guess the wrong letter one life will be deducted to total of lives the user have until the user runs out of lives before the user guess the correct word
+
